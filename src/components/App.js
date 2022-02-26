c6b8821584ec76b4c4f63ea9b3c20799a23379fc
Yash Maheshwari <Yash.Maheshwari@ibm.com>
2022-02-26 17:02:15 +0530

    implemented message functionalities

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,14 @@ import Messages from './Messages/Messages';
 import MetaPanel from './MetaPanel/MetaPanel';
 import SidePanel from './SidePanel/SidePanel';
 import { connect } from 'react-redux';
-const App = ({currentUser}) => (
+const App = ({currentUser, currentChannel}) => (
   <Grid columns="equal" className='app' style={{background:'#eee'}}>
     <ColorPanel/>
-    <SidePanel currentUser={currentUser}/>
+    <SidePanel key={currentUser && currentUser.uid} currentUser={currentUser}/>
 
 
   <Grid.Column style={{marginLeft: 320}}>
-    <Messages/>
+    <Messages key={currentChannel && currentChannel.id} currentChannel={currentChannel} currentUser={currentUser}/>
   </Grid.Column>
 
   <Grid.Column width={4}>
@@ -22,7 +22,8 @@ const App = ({currentUser}) => (
 </Grid>
 )
 const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+  currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel
 })
 
 export default connect(mapStateToProps)(App);
